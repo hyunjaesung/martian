73d9b0e8fda00177cf93eb363d65404894af2ae2
hyunjae <hyunjaesung@tmon.co.kr>
2020-02-26 16:46:00 +0900

    웹팩 및 레포 기본설정

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-martian_readingproject
+## martian_readingproject  
+  
+CRA 안쓰고 한큐에 타입스크립트와 sass 리액트까지 다 설정하려고 했으나 아직은 실패....
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { FC } from 'react';
+
+const App: FC = () => {
+  return <div>Hello TS React</div>;
+};
+
+export default App;
