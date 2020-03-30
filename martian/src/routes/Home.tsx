97cc6bf2001235ba585277e4b3c24a976ff5083d
hyunjae <steveysung91@gmail.com>
2020-03-31 07:48:28 +0900

    readme랑 여러준비

--- martian/src/routes/Home.tsx
+++ martian/src/routes/Home.tsx
@@ -10,83 +10,68 @@ export const bounceUp = keyframes`
   }`;
 
 export const brightUpDown = keyframes`
-0%{ opacity: 0.3;}
-50%{ opacity: 0.9;}
-100%{opacity:0.3;}
+    0%{ opacity: 0.3;}
+    50%{ opacity: 0.9;}
+    100%{opacity:0.3;}
 }`;
 
 const TitleContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	animation: ${bounceUp} 2s linear infinite;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  animation: ${bounceUp} 2s linear infinite;
+  &:hover {
+    animation: none;
+  }
 `;
 
 const HomeContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	text-align: center;
-	width: 100%;
-	height: 60%;
-`;
-const MainTitle = styled.h1`
-	margin: 0px;
-	height: 10.5rem;
-	/* width: 100%; */
-	font-family: Righteous;
-	font-size: 10rem;
-	text-align: justify;
-	color: white;
-	opacity: 0.9;
-`;
-const SubTitle = styled.h3`
-	margin: 0px;
-	/* width: 100%; */
-	font-family: Squada One;
-	font-weight: 1000;
-	font-size: 3rem;
-	text-align: justify;
-	color: white;
-	opacity: 0.6;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+  height: 60%;
 `;
 
-const SearchBar = styled.input`
-	margin-top: 50px;
-	width: 30%;
-	height: 60px;
-	text-align: center;
-	font-size: 2rem;
+const MainTitle = styled.h1`
+  margin: 0px;
+  height: 10.5rem;
+  /* width: 100%; */
+  font-family: Righteous;
+  font-size: 10rem;
+  text-align: justify;
+  color: white;
+  opacity: 0.9;
 `;
 
 const JourneyLink = styled.div`
-	margin-top: 20px;
-	font-family: 'Do Hyeon';
-	color: white;
-	opacity: 0.7;
-	transition: opacity 0.2s linear;
-	animation: ${brightUpDown} 2s linear infinite;
-	&:hover {
-		opacity: 0.9;
-		animation: none;
-	}
+  margin-top: 20px;
+  font-family: 'Do Hyeon';
+  font-size: 1.7rem;
+  color: white;
+  opacity: 0.7;
+  transition: opacity 0.2s linear;
+  animation: ${brightUpDown} 2s linear infinite;
+  &:hover {
+    opacity: 0.9;
+    animation: none;
+  }
 `;
 
 const Home: FC = () => {
-	return (
-		<HomeContainer>
-			<TitleContainer>
-				<MainTitle>Martian</MainTitle>
-				<SubTitle>Reading Project</SubTitle>
-			</TitleContainer>
-			<SearchBar />
-			<Link to="/journey">
-				<JourneyLink>Start Your Journey</JourneyLink>
-			</Link>
-		</HomeContainer>
-	);
+  return (
+    <HomeContainer>
+      <Link to="/journey">
+        <TitleContainer>
+          <MainTitle>Martian</MainTitle>
+        </TitleContainer>
+        <JourneyLink>Start Your Journey</JourneyLink>
+      </Link>
+    </HomeContainer>
+  );
 };
 
 export default Home;
